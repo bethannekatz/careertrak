ad179b8d2355e6651c44865d90d14e3e1d7e103b
bethannekatz <bkatz@andrew.cmu.edu>
2013-12-03 23:00:17 -0500

    adding forms and models

---
+++ careerapp/forms.py
@@ -0,0 +1,178 @@
+from django import forms
+
+from django.contrib.auth.models import User
+from models import *
+
+
+class JobListingForm(forms.ModelForm):
+	class Meta:
+		model = JobListing
+		exclude = ('company',)
+
+		
+
+
+class StudentRegistrationForm(forms.Form):
+    first_name = forms.CharField(max_length = 30,
+                                label="",
+                                widget = forms.TextInput(attrs={'class': 'form-control form-padding', 'placeholder':'First name'}))
+
+    last_name = forms.CharField(max_length = 30,
+                                label="",
+                                widget = forms.TextInput(attrs={'class': 'form-control form-padding', 'placeholder': 'Last name'}))
+                                
+    password1 = forms.CharField(max_length = 30,
+                                label="",
+                                widget = forms.PasswordInput(attrs={'class': 'form-control form-padding', 'placeholder': 'Password'}))
+
+    password2 = forms.CharField(max_length = 30,
+                                label="",
+                                widget = forms.PasswordInput(attrs={'class': 'form-control form-padding', 'placeholder': 'Confirm password'}))
+
+    emailaddress = forms.EmailField(max_length = 200,
+                                label="",   
+                                widget = forms.TextInput(attrs={'class': 'form-control form-padding', 'placeholder': 'School email address'}))
+
+    school = forms.ModelChoiceField(queryset=School.objects.all().order_by('name'),
+                                    label="School",
+                                    widget = forms.Select(attrs={'class': 'form-control form-padding', 'placeholder': 'School'}))
+
+    #Overrides the forms.Form.clean function.
+    def clean(self):
+        # Calls our parent (forms.Form) .clean function, gets a dictionary
+        # of cleaned data as a result
+        cleaned_data = super(StudentRegistrationForm, self).clean()
+
+        # Confirm that the two password fields match
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+        email = cleaned_data.get('emailaddress')
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords must match.")
+
+        print 'before'
+        school = cleaned_data.get('school')
+        print email
+
+        if not school:
+            raise forms.ValidationError("No school selected")
+
+        # TODO : Write a better function
+        if (school.email
+            not in email):
+             raise forms.ValidationError("Your email address does not match your school's")
+
+        if(User.objects.filter(email=email)):
+           raise forms.ValidationError("An account with this email address has already been registered")
+
+        # Return the cleaned data we got from our parent.
+        return cleaned_data
+		
+# USE ONE_TO_ONE 
+class PhotoForm(forms.ModelForm):
+	class Meta:
+		model = Photo
+		exclude = ('user', 'company')
+		widgets = {'picture' : forms.FileInput() }
+		
+class TranscriptForm(forms.ModelForm):
+	class Meta:
+		model = Transcript
+		exclude = ('user', )
+		widgets = {'transcript' : forms.FileInput() }
+		
+class LetterForm(forms.ModelForm):
+	class Meta:
+		model = Letter
+		exclude = ('user', )
+		widgets = {'letter' : forms.FileInput() }
+
+class RecruiterRegistrationForm(forms.Form):
+    first_name = forms.CharField(max_length = 30,
+                                label="",
+                                widget = forms.TextInput(attrs={'class': 'form-control form-padding', 'placeholder':'First name'}))
+
+    last_name = forms.CharField(max_length = 30,
+                                label="",
+                                widget = forms.TextInput(attrs={'class': 'form-control form-padding', 'placeholder': 'Last name'}))
+                                
+    password1 = forms.CharField(max_length = 30,
+                                label="",
+                                widget = forms.PasswordInput(attrs={'class': 'form-control form-padding', 'placeholder': 'Password'}))
+
+    password2 = forms.CharField(max_length = 30,
+                                label="",
+                                widget = forms.PasswordInput(attrs={'class': 'form-control form-padding', 'placeholder': 'Confirm password'}))
+
+    emailaddress = forms.EmailField(max_length = 200,
+                                label="",   
+                                widget = forms.TextInput(attrs={'class': 'form-control form-padding', 'placeholder': 'Email address'}))
+
+    company = forms.ModelChoiceField(queryset=Company.objects.all().order_by('name'),
+                                    label="Company",
+                                    widget = forms.Select(attrs={'class': 'form-control form-padding', 'placeholder': 'Company'}))
+
+    #Overrides the forms.Form.clean function.
+    def clean(self):
+        # Calls our parent (forms.Form) .clean function, gets a dictionary
+        # of cleaned data as a result
+        cleaned_data = super(RecruiterRegistrationForm, self).clean()
+
+        # Confirm that the two password fields match
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+        email = cleaned_data.get('emailaddress')
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords must match.")
+
+        print 'before'
+        company = cleaned_data.get('company')
+        print email
+
+        if not company:
+            raise forms.ValidationError("No company selected")
+
+
+        if(User.objects.filter(email=email)):
+           raise forms.ValidationError("An account with this email address has already been registered")
+
+        # Return the cleaned data we got from our parent.
+        return cleaned_data   
+
+
+class EmailResetPasswordForm(forms.Form):
+    emailaddress = forms.EmailField(max_length = 200,
+                                label='Enter the email address associated with your account',
+                                widget = forms.TextInput(attrs={'class': 'form-control'}))
+
+    # Customizes form validation for properties that apply to more
+    # than one field.  Overrides the forms.Form.clean function.
+    def clean(self):
+        # Calls our parent (forms.Form) .clean function, gets a dictionary
+        # of cleaned data as a result
+        cleaned_data = super(EmailResetPasswordForm, self).clean()
+        email = cleaned_data.get('emailaddress')
+        if not User.objects.filter(email__exact=email):
+            print "no"
+            raise forms.ValidationError("That email address was not found!")
+
+        # Return the cleaned data we got from our parent.
+        return cleaned_data
+
+class EditPasswordForm(forms.Form):
+        current = forms.CharField(max_length=30, label="", widget=forms.PasswordInput(attrs={'class': 'form-control form-padding', 'placeholder': 'Current Password'}))
+        password1 = forms.CharField(max_length=30, label="",widget=forms.PasswordInput(
+                attrs={'class': 'form-control form-padding', 'placeholder': 'New password'}))
+        password2 = forms.CharField(max_length=30, label="", widget=forms.PasswordInput(
+                attrs={'class': 'form-control form-padding', 'placeholder': 'Confirm new password'}))
+        # Customizes form validation for properties that apply to more
+        # than one field.  Overrides the forms.Form.clean function.
+        def clean(self):
+                cleaned_data = super(EditPasswordForm, self).clean()
+                password1 = cleaned_data.get('password1')
+                password2 = cleaned_data.get('password2')
+                if password1 and password2 and password1 != password2:
+                        raise forms.ValidationError("Passwords must match.")
+                return cleaned_data
--- careerapp/models.py
+++ careerapp/models.py
@@ -1,3 +1,174 @@
 from django.db import models
-
 # Create your models here.
+from django.contrib.auth.models import User
+import datetime
+
+class SearchResult(models.Model):
+    name = models.CharField(max_length=50)
+    tagline = models.CharField(max_length=50)
+    resultType = models.CharField(max_length=50)
+
+class School(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=30)
+    def __unicode__(self):
+        return self.name
+
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    rating = models.FloatField(max_length=100, default='0')
+    numRated = models.IntegerField(max_length=100, default='0')
+    location = models.CharField(max_length=100)
+    def __unicode__(self):
+        return self.name
+
+    def get_name(self):
+        return self.name
+
+    def get_tag(self):
+        return self.location
+
+    def get_resulttype(self):
+        return "Company"
+           
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='userProfile')
+    school = models.ForeignKey(School, blank=True, null=True)
+    company = models.ForeignKey(Company, blank=True, null=True, related_name="company")
+    following = models.ManyToManyField(Company, blank=True, related_name="following", through='FollowingCompany')
+    connectRequests = models.ManyToManyField(User, blank=True, related_name="connectRequests")
+    connections = models.ManyToManyField(User, blank=True, related_name="connections")
+    location = models.CharField(max_length=50, blank=True)
+    degree = models.CharField(max_length=50, blank=True)
+    prevJobs = models.CharField(max_length=500, blank=True)
+    currJobs = models.CharField(max_length=500, blank=True)
+
+
+    STUDENT = 'student'
+    RECRUITER = 'recruiter'
+    
+    USER_TYPE_CHOICES = (
+        (STUDENT, 'Student'),
+        (RECRUITER, 'Recruiter'),
+    )
+
+    
+    userType = models.CharField(max_length=2, choices = USER_TYPE_CHOICES, default = STUDENT)
+
+    #TODO ask TA about syntax
+
+    def is_student(self):
+        return self.userType in (self.STUDENT)
+
+    def is_recruiter(self):
+        return self.userType in (self.RECRUITER)
+    
+    def __unicode__(self):
+        return self.user.username
+
+    def get_name(self):
+        return self.user.first_name + " " +self.user.last_name
+
+    def get_tag(self):
+        if self.userType in (self.STUDENT):
+            return "A student at "+ str(self.school)
+        else:
+            return "A recruiter at "+ str(self.company)
+        
+
+    def get_resulttype(self):
+        return self.userType
+		
+class FollowingCompany(models.Model):
+	company = models.ForeignKey(Company)
+	userProfile = models.ForeignKey(UserProfile)
+
+# Represents a connection, which may or may not be acceped by both parties
+class Connection(models.Model):
+	user = models.ForeignKey(User)
+	userProfile = models.ForeignKey(UserProfile)
+	isValid = models.BooleanField()
+
+class Photo(models.Model):
+	picture = models.ImageField(upload_to="profilePics", blank=True)
+	user = models.ForeignKey(User)
+	company = models.ForeignKey(Company, blank=True, null=True)
+	def __unicode__(self):
+		return str(self.id)
+
+class JobListing(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.CharField(max_length = 1000)
+    company = models.ForeignKey(Company, blank=False, related_name='postings')
+    creation_time = models.DateTimeField(default=datetime.datetime.now, editable=False)
+
+    location = models.CharField(max_length=75, blank=True)
+    category = models.CharField(max_length=50, blank=True)
+    division = models.CharField(max_length=50, blank=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_resulttype(self):
+        return "Job Posting"
+
+    def get_name(self):
+        return self.title
+
+class Transcript(models.Model):
+	transcript = models.FileField(upload_to="transcripts", blank=True)
+	user = models.ForeignKey(User)
+	
+class Letter(models.Model):
+	letter = models.FileField(upload_to="letters", blank=True)
+	user = models.ForeignKey(User)
+
+
+
+##class CommonResume(models.Model):
+##    experience = models.CharField(max_length=500)
+##    skills = models.CharField(max_length=500)
+##    gpa = models.CharField(max_length=4, blank=True)
+##
+##    def __unicode__(self):
+##        return self.experience
+##
+### Represents a institute of higher education.
+### Is a ForeignKey on StudentUser
+##class School(models.Model):
+##    name = models.CharField(max_length=80)
+##    def __unicode__(self):
+##        return self.name
+##
+##
+### A company whose recruiters are registred on the site
+##class Company(models.Model):
+##    name = models.CharField(max_length=80)
+##    
+##    def __unicode__(self):
+##        return self.name
+##
+### Represents a studentUser associated with an existing school and user
+##class StudentUser(models.Model):
+##    user = models.OneToOneField(User)
+##    school = models.ForeignKey(School)
+##    resume = models.OneToOneField(CommonResume)
+##    
+##    def __unicode__(self):
+##	return self.user.username
+##
+### Represents a recruiterUser associated with an existing company and user
+##class RecruiterUser(models.Model):
+##    user = models.OneToOneField(User)
+##    company = models.ForeignKey(Company)
+##    school = models.ManyToManyField(School, blank = True)
+##    def __unicode__(self):
+##	return self.user.username  +" Company: " + self.company
+##
+
+##
+
+
