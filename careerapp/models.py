3354ed4c869df497207b25f245c8f7b7f8bd8839
bethannekatz <bkatz@andrew.cmu.edu>
2013-12-04 22:37:41 -0500

    changed min length

--- careerapp/models.py
+++ careerapp/models.py
@@ -56,7 +56,7 @@ class UserProfile(models.Model):
     )
 
     
-    userType = models.CharField(max_length=2, choices = USER_TYPE_CHOICES, default = STUDENT)
+    userType = models.CharField(max_length=20, choices = USER_TYPE_CHOICES, default = STUDENT)
 
     #TODO ask TA about syntax
 
