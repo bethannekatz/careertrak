f1ac733461d2b05afdc0ec06758b7e4476905de9
bethannekatz <bkatz@andrew.cmu.edu>
2013-12-06 14:21:27 -0500

    edited settings.py

--- career_trak/settings.py
+++ career_trak/settings.py
@@ -86,6 +86,13 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Email
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_HOST_USER = 'car
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
